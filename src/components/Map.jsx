24937ad43a325c4853b32ede73971ab93459d82d
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-08-17 11:25:04 -0500

    [add] popup box for marker click

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,52 +1,38 @@
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { GoogleMap, InfoWindow, Marker, withGoogleMap } from 'react-google-maps';
 import { default as React, Component } from 'react';
+import firebase from 'firebase';
+import geocoder from 'google-geocoder';
 // import InfoWindow from './InfoWindow.jsx';
+
+const googleMapUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAEcsBoANLQ0cs7xmx0UJXpdLRLOiFHGps';
 
 const SimpleGoogleMap = withGoogleMap(props => (
   <GoogleMap
-    googleMapUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEcsBoANLQ0cs7xmx0UJXpdLRLOiFHGps"
-    defaultZoom={10}
+    googleMapUrl={googleMapUrl}
+    defaultZoom={11}
     defaultCenter={{ lat: 29.946612, lng: -90.070113 }}
   >
      {props.markers.map((marker, index) => {
-       const onClick = () => props.onMarkerClick(marker);
-       /* const onCloseClick = () => props.onCloseClick(marker); */
        return (
          <Marker
            key={index}
            position={marker.position}
-           title={(index + 1).toString()}
-           onClick={onClick}
+           onClick={() => props.onMarkerClick(marker)}
          >
-          {/* {marker.showInfo && (
-            <InfoWindow onCloseClick={onCloseClick}>
-              <div>
-                <strong>{marker.content}</strong>
-                <br />
-                <em>The contents of this InfoWindow are actually ReactElements.</em>
-              </div>
-            </InfoWindow>
-          )} */}
+           {marker.showInfo && (
+             <InfoWindow onCloseClick={() => props.onMarkerClose(marker)}>
+               <div>
+                 <strong>{marker.content[0]}</strong>
+                 <br />
+                 <em>Instrument: {marker.content[1]}</em>
+               </div>
+             </InfoWindow>
+          )}
          </Marker>
       );
      })}
   </GoogleMap>
 ));
-
-function generateInitialMarkers() {
-  const positions = [{ lat: 29.976617, lng: -90.070113 }, { lat: 29.946612, lng: -90.370117 }, { lat: 29.996608, lng: -90.070113 }, { lat: 29.946612, lng: -90.470108 }, { lat: 29.846620, lng: -90.070113 }];
-
-  const markers = [];
-  for (let i = 0; i < 5; i++) {
-    const position = positions[i];
-    markers.push({
-      position,
-      content: 'This is the secret message'.split(' ')[i],
-      showInfo: false,
-    });
-  }
-  return markers;
-}
 
 export default class Map extends Component {
   constructor(props) {
@@ -56,12 +42,46 @@ export default class Map extends Component {
     };
     this.handleMarkerClick = this.handleMarkerClick.bind(this);
     this.handleCloseClick = this.handleCloseClick.bind(this);
+    this.getLocations = this.getLocations.bind(this);
   }
 
   componentDidMount() {
-    this.setState({
-      markers: generateInitialMarkers(),
-    });
+    this.getLocations();
+  }
+  getLocations() {
+    const temp = [];
+    const groupMarkers = [];
+    firebase.database().ref('/groups/')
+      .on('value', snapshot => {
+        const snap = snapshot.val();
+        for (const prop in snap) {
+          temp.push(snap[prop]);
+        }
+        temp.forEach(el => {
+          for (const prop in el) {
+            const instrument = el[prop].instrument;
+            const name = el[prop].name;
+            const geo = geocoder({
+              key: 'AIzaSyAEcsBoANLQ0cs7xmx0UJXpdLRLOiFHGps',
+            });
+            geo.find(el[prop].loc, (err, res) => {
+              if (err) {
+                console.error(err);
+              } else {
+                const position = res[0].location;
+                groupMarkers.push({
+                  position,
+                  showInfo: false,
+                  content: [name, instrument],
+                });
+                this.setState({
+                  markers: groupMarkers,
+                });
+              }
+            });
+          }
+        });
+      });
   }
 
   handleMarkerClick(targetMarker) {
