30483727c005aa77dcdc171309f60ce72de89886
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-08-18 11:29:31 -0500

    [add]events to calendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
+import BigCalendar from 'react-big-calendar';
+import style from 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+import axios from 'axios';
+
+const CALENDAR_ID = 'lfhaqtrds93ei95e6g7qrbf0no%40group.calendar.google.com';
+const API_KEY = 'AIzaSyAEcsBoANLQ0cs7xmx0UJXpdLRLOiFHGps';
+const url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`;
+
+
+BigCalendar.momentLocalizer(moment);
 
 export default class Calendar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      events: [],
+    };
+    this.getEvents = this.getEvents.bind(this);
+  }
+  componentDidMount() {
+    this.getEvents((e) => {
+      this.setState({
+        events: e,
+      });
+    });
+  }
+  getEvents(callback) {
+    axios
+    .get(url)
+    .then((res) => {
+      const events = [];
+      res.data.items.forEach(event => {
+        events.push({
+          start: event.start.date || event.start.dateTime,
+          end: event.end.date || event.end.dateTime,
+          title: event.summary,
+        });
+      });
+      callback(events);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
   render() {
     return (
       <div className="container">
-        <iframe
-          src="https://calendar.google.com/calendar/embed?src=lfhaqtrds93ei95e6g7qrbf0no%40group.calendar.google.com&ctz=America/Chicago"
-          style={{ border: '0px', width: '800px', height: '600px', frameBorder: '0px', scrolling: 'no' }}
-        >
-        </iframe>
+        <BigCalendar
+          style={{ style, height: '600px' }}
+          events={this.state.events}
+        />
       </div>
     );
   }
