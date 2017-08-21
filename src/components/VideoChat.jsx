e4335dd00e8718eb7859ef8c48c71868bebf3f10
Jmetev1 <jmetevier@gmail.com>
2017-08-21 14:53:58 -0500

    [add] styling to videochat

--- src/components/VideoChat.jsx
+++ src/components/VideoChat.jsx
@@ -8,12 +8,21 @@ export class TokBoxChat extends React.Component {
     const them = props.props;
     const combo = [me, them].sort().join('-');
     this.url = `https://tokbox.com/embed/embed/ot-embed.js?embedId=7f122061-a137-4a5f-8e12-90adc74dd8e4&room=${combo}&iframe=true`;
+    console.log(this.url);
   }
   render() {
     return (
-      <div className="iframe">
-        <iframe className="iframe" src={this.url}></iframe>
-      </div>);
+      <div>
+        <div className="vchatContainer">
+          <div>
+            <h2>Now chatting with {this.props.props.toUpperCase()[0] + this.props.props.slice(1)}</h2>
+          </div>
+        </div>
+        <div className="iframe">
+          <iframe className="iframe" src={this.url}></iframe>
+        </div>
+      </div>
+      );
   }
 }
 export default TokBoxChat;
--- src/components/VideoChatContainer.jsx
+++ src/components/VideoChatContainer.jsx
@@ -36,20 +36,20 @@ export class VideoChatContainer extends React.Component {
     const loggedUsers = this.state.users.map(user => (
       <div>
         <button
-          className="messageStyle"
           onClick={this.video.bind(null, user)} key={user}>
-          <h2><strong>{user.toUpperCase()}</strong></h2>
-        </button>< br />< br />
+          <h2><strong>{user.toUpperCase()[0] + user.slice(1)}</strong></h2>
+        </button>< br />
       </div>));
     return (
       <div className="chat">
-        <div className="col-md-4">
-          <h2>Who's Available To Jam Live?</h2>
+        <div className="col-md-4 vchatTitle">
+          <h2>Who's Available To</h2>
+          <h2>Jam Live?</h2>
           <h4>Click A Name To Jam Together!</h4>
           <div>{loggedUsers}</div>
         </div>
         <div>
-          <div className="videoChat col-md-8">
+          <div className="videoChat col-md-8 vchatTitle">
             {this.state.showVideo ? <TokBoxChat props={this.state.chosen} /> :
               <h1 className="videoWait">Please Choose A User To Live Jam With</h1>}
           </div>
